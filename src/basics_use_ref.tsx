c0e7d861be00f82f76496a7eaff6c44366845e0b
pshaddel <p.shaddel@gmail.com>
2024-04-01 11:05:50 +0200

    most common use case of useRef

--- src/basics_use_ref.tsx
+++ src/basics_use_ref.tsx
@@ -3,15 +3,20 @@ import { useEffect, useRef, useState } from "react";
 export function BasicUseRef() {
     const [name, setName] = useState('');
     const renderCount = useRef(0);
+    const inputRef = useRef(null);
     useEffect(() => {
         renderCount.current++
     })
 
     return (
         <>
-        <input value={name} onChange={e => setName(e.target.value)} />
+        <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
         <div>My name is {name}</div>
         <div>Number of Rerenders {renderCount.current}</div>
+        <button onClick={() => {
+            if (!inputRef) return;
+            (inputRef.current as any).focus();
+        }}>Focus on Input using useRef</button>
         </>
     )
-}
+} 
